af067acd4d0af26024059551d7336b3a9dc774fb
LoggerPro <460587@bsd48.org>
2019-11-21 12:24:46 -0800

    Added video capture functionality

--- MotionCapture.py
+++ MotionCapture.py
@@ -6,12 +6,28 @@ Created on Tue Nov 19 11:54:09 2019
 
 This code will record an image of the joint of interest from a webcam
 """
-#This has been a sample github edit
-#This has been another sample github edit, this time with a specified commit
-
 #import need libraries
+import cv2
 
-#capture an image from the camera\
+#capture an image from the camera
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
 
-#
-#SAMPLE SAMPLE SAMPLE CHANGE
+#create window to display image
+cv2.namedWindow('cap')
+
+
+keypressed = 1
+while keypressed != 27:
+    #read a frame from the video capture
+    ret, frame = cap.read()
+    
+    cv2.imshow('cap',frame)
+    
+    #wait for button press
+    keypressed = cv2.waitKey(1)
+    
+if keypressed == 27:
+    #destroy windows
+    cv2.destroyAllWindows()
+    cap.release()
