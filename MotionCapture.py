cb95e016636319a00c34a59df65a1ee885bb88dc
LoggerPro <460587@bsd48.org>
2019-11-21 11:17:49 -0800

    Initial commit

---
+++ MotionCapture.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 11:54:09 2019
+
+@author: Samuel Rothstein
+
+This code will record an image of the joint of interest from a webcam
+"""
+
+#import need libraries
+
+#capture an image from the camera\
+
+#
