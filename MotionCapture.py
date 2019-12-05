ea38362f7ba0db678a33ef7bb491dcfb254ab572
LoggerPro <460587@bsd48.org>
2019-12-05 11:56:16 -0800

    Added combined filtered image

--- MotionCapture.py
+++ MotionCapture.py
@@ -11,21 +11,34 @@ import cv2
 import numpy
 import os.path
 
+"""Filter Settings for different colors in form of min[H,S,V] max[H,S,V]:
+Green: [77,32,0] [101,202,255]
+Yellow: [20,73,75] [303,255,255]
+Blue: [86,96,78] [117,227,255]
+"""
 #capture an image from the camera; 0 = built in webcam or external camera
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 
-color0 = numpy.array([0,0,0])
-color1 = numpy.array([0,0,0])
-color2 = numpy.array([0,0,0])
-color3 = numpy.array([0,0,0])
+#Filter ranges for each color (HSV)
+color0_min = numpy.array([77,32,0])
+color0_max = numpy.array([101,202,255])
+color1_min = numpy.array([20,73,75])
+color1_max = numpy.array([30,255,255])
+color2_min = numpy.array([86,96,78])
+color2_max = numpy.array([117,227,255])
 
 #create windows to display image (filtered and original)
 cv2.namedWindow('frame')
+cv2.namedWindow('filter')
+
+"""
 cv2.namedWindow('rgbFilter')
 cv2.namedWindow('hsvFilter')
+
 cv2.namedWindow('min')
 cv2.namedWindow('max')
+
 
 #Min RGB
 cv2.createTrackbar('minR','min', 0, 255, lambda x:None)
@@ -44,6 +57,7 @@ cv2.createTrackbar('minV','min', 0, 255, lambda x:None)
 cv2.createTrackbar('maxH','max', 180, 180, lambda x:None)
 cv2.createTrackbar('maxS','max', 255, 255, lambda x:None)
 cv2.createTrackbar('maxV','max', 255, 255, lambda x:None)
+"""
 
 keypressed = 1
 while keypressed != 27:
@@ -52,6 +66,7 @@ while keypressed != 27:
     #Create HSV converted frame
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
+    """
     #create lower and upper bounds using trackbar position for RGB and HSV
     lowerRGB = numpy.array([cv2.getTrackbarPos('minB','min'), cv2.getTrackbarPos('minG','min'), cv2.getTrackbarPos('minR','min')])
     upperRGB = numpy.array([cv2.getTrackbarPos('maxB','max'), cv2.getTrackbarPos('maxG','max'), cv2.getTrackbarPos('maxR','max')])
@@ -65,12 +80,31 @@ while keypressed != 27:
     
     maskHSV = cv2.inRange(hsv, lowerHSV, upperHSV)
     filterHSV = cv2.bitwise_and(frame,frame, mask=maskHSV)
+    """
+    
+    #Create masks for each color using their predeclared restrictions
+    color0_mask = cv2.inRange(hsv, color0_min, color0_max)
+    color1_mask = cv2.inRange(hsv, color1_min, color1_max)
+    color2_mask = cv2.inRange(hsv, color2_min, color2_max)
+    
+    #Create filtered image for each individual color
+    color0_filter = cv2.bitwise_and(frame,frame, mask=color0_mask)
+    color1_filter = cv2.bitwise_and(frame,frame, mask=color1_mask)
+    color2_filter = cv2.bitwise_and(frame,frame, mask=color2_mask)
+    
+    #Combine each filtered image to have one master filter
+    filtered = cv2.bitwise_or(cv2.bitwise_or(color0_filter, color1_filter), color2_filter)
     
     #show frame and filtered images
     cv2.imshow('frame',frame)
+    cv2.imshow('filter',filtered)
+    cv2.imshow('test0',color0_filter)
+    cv2.imshow('test1',color1_filter)
     
+    """
     cv2.imshow('rgbFilter',filterRGB)
     cv2.imshow('hsvFilter',filterHSV)
+    """
     
     #wait for button press
     keypressed = cv2.waitKey(1)
