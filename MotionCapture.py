6b5fe291f4cbd11cf7d94fe8b2b948ab2ac6ac57
LoggerPro <460587@bsd48.org>
2020-02-14 12:22:32 -0800

    Working centroids and line connections, added erode/dilate function to class, changed filter parameters

--- MotionCapture.py
+++ MotionCapture.py
@@ -29,6 +29,12 @@ Blue: [106,0,0] [126,0,0]
 Red: [3,155,0] [15,255,255]
 Green: [70,60,80] [110,170,255]
 Yellow: [20,70,0] [35,255,255]
+
+Third try Buttons:
+Blue: [105,50,5] [120,255,200]
+Red: [5,200,50] [15,255,255]
+Green: [60,50,0] [100,200,255]
+Yellow: [20,130,80] [35,255,255]
 """
 
 class Color():
@@ -41,27 +47,25 @@ class Color():
         self.filter = cv2.bitwise_and(frame,frame, mask=mask)
         return(self.filter)
 
-    def centroid(self, hsv, frame):
+    def centroid(self, hsv, frame, e, d):
         # Find centroid of dilated image
-        image = self.color_filter(hsv, frame)
-        kernel = numpy.ones((5,5), numpy.uint8)
-        eroded = cv2.erode(image, kernel, iterations=3)
-        dilated = cv2.dilate(eroded, kernel, iterations=7)
-        #eroded = cv2.erode(dilated, kernel, iterations=5)
+        #image = self.color_filter(hsv, frame)
+        #kernel = numpy.ones((5,5), numpy.uint8)
+        #eroded = cv2.erode(image, kernel, iterations=3)
         #dilated = cv2.dilate(eroded, kernel, iterations=7)
-        image = dilated
+        #image = dilated
+        # Erode/dilate the image
+        image = self.erode_dilate(hsv, frame, e, d)
+        
         
         # Make it grayscale, threshold it, find the moments
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(gray_image,0,255,0)
-        cv2.imshow('test',thresh)
         M = cv2.moments(thresh)
-        print(M["m00"])
         # So we don't divide by zero
         if(M["m00"] != 0):
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
-            print("X:" + str(cX) + "  Y:" + str(cY))
         else:
             if ('cX' in globals()) and ('cY' in globals()):
                 pass
@@ -69,7 +73,16 @@ class Color():
                 cX, cY = 0, 0
         # Make the circle in the coordinates
         cv2.circle(image, (cX, cY), 5, (255,255,255), -1)
+        self.centroid_x, self.centroid_y = cX, cY
         return(image)
+        
+    def erode_dilate(self, hsv, frame, e, d):
+        # erodes and dilates with set iterations
+        image = self.color_filter(hsv, frame)
+        kernel = numpy.ones((5,5), numpy.uint8)
+        eroded = cv2.erode(image, kernel, iterations=e)
+        dilated = cv2.dilate(eroded, kernel, iterations=d)
+        return(dilated)
 
 def run_filter():
     # Capture an image from the camera; 0 = built in webcam or external camera
@@ -81,14 +94,15 @@ def run_filter():
     cv2.namedWindow('filter')
     cv2.namedWindow('erode/dilate')
     cv2.namedWindow('centroid')
+    cv2.namedWindow('connections')
     
     keypressed = 1
     
     # Initialize colors
-    color0 = Color(numpy.array([106,0,0]), numpy.array([126,255,255]))
-    color1 = Color(numpy.array([3,155,0]), numpy.array([15,255,255]))
-    color2 = Color(numpy.array([70,60,80]), numpy.array([110,170,255]))
-    color3 = Color(numpy.array([20,70,0]), numpy.array([35,225,255]))
+    color0 = Color(numpy.array([105,50,5]), numpy.array([120,255,200]))
+    color1 = Color(numpy.array([5,200,50]), numpy.array([15,255,255]))
+    color2 = Color(numpy.array([60,50,30]), numpy.array([100,200,255]))
+    color3 = Color(numpy.array([20,130,80]), numpy.array([35,255,255]))
     
     while keypressed != 27:
         # Read a frame from the video capture
@@ -106,19 +120,35 @@ def run_filter():
         # Combine all color filters
         filtered = cv2.bitwise_or(cv2.bitwise_or(cv2.bitwise_or(color0_filter, color1_filter), color2_filter), color3_filter)
         
-        # Erode/Dilate
-        kernel = numpy.ones((5,5), numpy.uint8)
-        eroded = cv2.erode(filtered, kernel, iterations=3)
-        dilated = cv2.dilate(eroded, kernel, iterations=5)
+        # Erode/dilate individual colors
+        color0_erode_dilate = color0.erode_dilate(hsv, frame, 3, 10)
+        color1_erode_dilate = color1.erode_dilate(hsv, frame, 2, 10)
+        color2_erode_dilate = color2.erode_dilate(hsv, frame, 2, 10)
+        color3_erode_dilate = color3.erode_dilate(hsv, frame, 2, 10)
+        
+        # Combine all
+        erode_dilate = cv2.bitwise_or(cv2.bitwise_or(cv2.bitwise_or(color0_erode_dilate, color1_erode_dilate), color2_erode_dilate), color3_erode_dilate)
         
         # Find centroids
-        centroid = color3.centroid(hsv, frame)
+        color0_centroid = color0.centroid(hsv, frame, 3, 10)
+        color1_centroid = color1.centroid(hsv, frame, 3, 10)
+        color2_centroid = color2.centroid(hsv, frame, 3, 10)
+        color3_centroid = color3.centroid(hsv, frame, 3, 10)
+        
+        # Combine centroids
+        centroid = cv2.bitwise_or(cv2.bitwise_or(cv2.bitwise_or(color0_centroid, color1_centroid), color2_centroid), color3_centroid)
+        
+        #Draw lines between centroids
+        connections = centroid
+        cv2.line(connections, (color0.centroid_x, color0.centroid_y), (color2.centroid_x, color2.centroid_y), (255,255,255), 5)
+        cv2.line(connections, (color1.centroid_x, color1.centroid_y), (color3.centroid_x, color3.centroid_y), (255,255,255), 5)
         
         # Show images
         cv2.imshow('frame', frame)
         cv2.imshow('filter', filtered)
-        cv2.imshow('erode/dilate', dilated)
+        cv2.imshow('erode/dilate', erode_dilate)
         cv2.imshow('centroid', centroid)
+        cv2.imshow('connections', connections)
         
         # Wait for button press
         keypressed = cv2.waitKey(1)
