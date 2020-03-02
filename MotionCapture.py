a0ceba1c8dcaf1beb62fe28d790539f2e8be4d46
LoggerPro <460587@bsd48.org>
2020-03-02 12:22:17 -0800

    Calculating intersection accurately

--- MotionCapture.py
+++ MotionCapture.py
@@ -10,6 +10,7 @@ This code will record an image of the joint of interest from a webcam
 import cv2
 import numpy
 import os.path
+import math
 
 """
 Filter Settings for different colors in form of min[H,S,V] max[H,S,V]:
@@ -31,23 +32,26 @@ Green: [70,60,80] [110,170,255]
 Yellow: [20,70,0] [35,255,255]
 
 Third try Buttons:
-Blue: [105,50,5] [120,255,200]
-Red: [5,200,50] [15,255,255]
+Blue: [105,100,5] [120,255,200]
 Green: [60,50,0] [100,200,255]
+Red: [5,200,50] [15,255,255]
 Yellow: [20,130,80] [35,255,255]
+
+
 """
 
 class Color():
     def __init__(self, color_min, color_max):
         self.min = color_min
         self.max = color_max
+        self.centroid = (0,0)
         
     def color_filter(self, hsv, frame):
         mask = cv2.inRange(hsv, self.min, self.max)
         self.filter = cv2.bitwise_and(frame,frame, mask=mask)
         return(self.filter)
 
-    def centroid(self, hsv, frame, e, d):
+    def find_centroid(self, hsv, frame, e, d):
         # Find centroid of dilated image
         #image = self.color_filter(hsv, frame)
         #kernel = numpy.ones((5,5), numpy.uint8)
@@ -57,23 +61,15 @@ class Color():
         # Erode/dilate the image
         image = self.erode_dilate(hsv, frame, e, d)
         
-        
         # Make it grayscale, threshold it, find the moments
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(gray_image,0,255,0)
         M = cv2.moments(thresh)
         # So we don't divide by zero
         if(M["m00"] != 0):
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-        else:
-            if ('cX' in globals()) and ('cY' in globals()):
-                pass
-            else:
-                cX, cY = 0, 0
+            self.centroid = ( int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]) )
         # Make the circle in the coordinates
-        cv2.circle(image, (cX, cY), 5, (255,255,255), -1)
-        self.centroid_x, self.centroid_y = cX, cY
+        cv2.circle(image, (self.centroid[0], self.centroid[1]), 5, (255,255,255), -1)
         return(image)
         
     def erode_dilate(self, hsv, frame, e, d):
@@ -99,10 +95,13 @@ def run_filter():
     keypressed = 1
     
     # Initialize colors
-    color0 = Color(numpy.array([105,50,5]), numpy.array([120,255,200]))
-    color1 = Color(numpy.array([5,200,50]), numpy.array([15,255,255]))
-    color2 = Color(numpy.array([60,50,30]), numpy.array([100,200,255]))
-    color3 = Color(numpy.array([20,130,80]), numpy.array([35,255,255]))
+    # 0-Blue, 1-Green, 2-Red, 3-Yellow
+    color0 = Color(numpy.array([105,100,25]), numpy.array([120,255,200]))
+    color1 = Color(numpy.array([60,50,30]), numpy.array([100,200,255]))
+    color2 = Color(numpy.array([5,210,50]), numpy.array([15,255,255]))
+    color3 = Color(numpy.array([20,130,80]), numpy.array([35,255,170]))
+    
+    intersection = [0,0]
     
     while keypressed != 27:
         # Read a frame from the video capture
@@ -121,27 +120,44 @@ def run_filter():
         filtered = cv2.bitwise_or(cv2.bitwise_or(cv2.bitwise_or(color0_filter, color1_filter), color2_filter), color3_filter)
         
         # Erode/dilate individual colors
-        color0_erode_dilate = color0.erode_dilate(hsv, frame, 3, 10)
+        color0_erode_dilate = color0.erode_dilate(hsv, frame, 4, 10)
         color1_erode_dilate = color1.erode_dilate(hsv, frame, 2, 10)
-        color2_erode_dilate = color2.erode_dilate(hsv, frame, 2, 10)
-        color3_erode_dilate = color3.erode_dilate(hsv, frame, 2, 10)
+        color2_erode_dilate = color2.erode_dilate(hsv, frame, 3, 10)
+        color3_erode_dilate = color3.erode_dilate(hsv, frame, 3, 10)
         
         # Combine all
         erode_dilate = cv2.bitwise_or(cv2.bitwise_or(cv2.bitwise_or(color0_erode_dilate, color1_erode_dilate), color2_erode_dilate), color3_erode_dilate)
         
-        # Find centroids
-        color0_centroid = color0.centroid(hsv, frame, 3, 10)
-        color1_centroid = color1.centroid(hsv, frame, 3, 10)
-        color2_centroid = color2.centroid(hsv, frame, 3, 10)
-        color3_centroid = color3.centroid(hsv, frame, 3, 10)
+        # Each centroid image
+        color0_centroid = color0.find_centroid(hsv, frame, 4, 10)
+        color1_centroid = color1.find_centroid(hsv, frame, 2, 10)
+        color2_centroid = color2.find_centroid(hsv, frame, 3, 10)
+        color3_centroid = color3.find_centroid(hsv, frame, 3, 10)
         
-        # Combine centroids
+        # Combine centroids into one image
         centroid = cv2.bitwise_or(cv2.bitwise_or(cv2.bitwise_or(color0_centroid, color1_centroid), color2_centroid), color3_centroid)
         
         #Draw lines between centroids
         connections = centroid
-        cv2.line(connections, (color0.centroid_x, color0.centroid_y), (color2.centroid_x, color2.centroid_y), (255,255,255), 5)
-        cv2.line(connections, (color1.centroid_x, color1.centroid_y), (color3.centroid_x, color3.centroid_y), (255,255,255), 5)
+        cv2.line(connections, (color0.centroid[0], color0.centroid[1]), (color1.centroid[0], color1.centroid[1]), (255,255,255), 5)
+        cv2.line(connections, (color2.centroid[0], color2.centroid[1]), (color3.centroid[0], color3.centroid[1]), (255,255,255), 5)
+        
+        # Calculate intersection point
+        if( (color1.centroid[0] - color0.centroid[0]) != 0 and (color3.centroid[0] - color2.centroid[0]) != 0 and (color01_slope - color23_slope) != 0):
+            color01_slope = float(color1.centroid[1] - color0.centroid[1]) / (color1.centroid[0] - color0.centroid[0])
+            color23_slope = float(color3.centroid[1] - color2.centroid[1]) / (color3.centroid[0] - color2.centroid[0])
+            intersection[0] = (-color1.centroid[1] + color3.centroid[1] + color01_slope*color1.centroid[0] - color23_slope*color3.centroid[0])/(color01_slope - color23_slope)
+            intersection[1] = color01_slope*(intersection[0]-color1.centroid[0]) + color1.centroid[1]
+            cv2.circle(connections, (int(intersection[0]), int(intersection[1])), 5, (255,255,255), -1)
+        
+        # Calculate angles (points are color0, color2, and intersection)
+        if( (intersection[0] - color0.centroid[0]) != 0 and (intersection[0] - color2.centroid[0]) != 0):
+            a = math.degrees( math.atan( (intersection[1] - color0.centroid[1]) / (intersection[0] - color0.centroid[0]) ) )
+            b = math.degrees( math.atan( (intersection[1] - color2.centroid[1]) / (intersection[0] - color2.centroid[0]) ) )
+            theta = 180 - a - b
+            print(a)
+            print(b)
+            print(theta)
         
         # Show images
         cv2.imshow('frame', frame)
@@ -203,6 +219,5 @@ def find_filter():
         # Destroy windows
         cv2.destroyAllWindows()
         cap.release()
-
 
 run_filter()
